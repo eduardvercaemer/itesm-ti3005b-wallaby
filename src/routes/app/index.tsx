6846a6db5824395cb3e11349f95bc00baae88e1d
ed <vercaemereduard@gmail.com>
2024-03-04 21:48:34 -0600

    basic styles

--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -9,7 +9,7 @@ export const useNotionLoader = routeLoader$(async (e) => {
   const db = database(e);
   const no = notion(e);
 
-  return getScheduleDetails(db, no, { forceReload: true });
+  return getScheduleDetails(db, no);
 });
 
 export default component$(() => {
@@ -20,28 +20,44 @@ export default component$(() => {
       <h1>maestros</h1>
       <ul class="flex gap-2">
         {notionData.value.teachers.map((i) => (
-          <li key={i}>{i}</li>
+          <li key={i} class="badge badge-accent">
+            {i}
+          </li>
         ))}
       </ul>
 
       <h1>clases</h1>
       <div class="overflow-x-auto">
-        <table class="table">
+        <table class="table table-zebra">
           <thead>
             <tr>
               <th>Clase</th>
+              <th>Días</th>
               <th>Inicio</th>
               <th>Fin</th>
               <th>Maestro</th>
+              <th>Grado</th>
+              <th>Salón</th>
             </tr>
           </thead>
           <tbody>
             {notionData.value.schedule.map((i) => (
               <tr key={i.id}>
                 <td>{i.title}</td>
+                <td class="flex gap-1">
+                  {i.day.map((d) => (
+                    <span class="badge">{d}</span>
+                  ))}
+                </td>
                 <td>{i.start}</td>
                 <td>{i.end}</td>
-                <td>{i.teacher}</td>
+                <td>
+                  {i.teacher.map((t) => (
+                    <span class="badge">{t}</span>
+                  ))}
+                </td>
+                <td>{i.grade}</td>
+                <td>{i.room}</td>
               </tr>
             ))}
           </tbody>
