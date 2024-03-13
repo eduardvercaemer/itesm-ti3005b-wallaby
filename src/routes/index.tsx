afde27343ec615503204c8d7c04df450b8dff4dc
ed <vercaemereduard@gmail.com>
2024-03-13 11:29:37 -0600

    prefetch on session

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,11 @@ import { useLocation } from "@builder.io/qwik-city";
 import { Link } from "@builder.io/qwik-city";
 
 import { LoadingScreen } from "~/components/loading/loading-screen";
+import { useAuthSession } from "~/routes/plugin@10-auth";
 
 export default component$(() => {
   const location = useLocation();
+  const session = useAuthSession();
 
   if (location.isNavigating) return <LoadingScreen />;
 
@@ -16,7 +18,11 @@ export default component$(() => {
         <div class="hero-content text-center">
           <div class="max-w-md">
             <h1 class="text-5xl font-bold">Wallaby</h1>
-            <Link prefetch={false} href="/app" class="btn btn-primary">
+            <Link
+              prefetch={session.value !== null}
+              href="/app"
+              class="btn btn-primary"
+            >
               Ir a Horarios
             </Link>
           </div>
