e0c0f66d85f6baee2194e938d8a98b8f2ce8a278
ed <vercaemereduard@gmail.com>
2024-03-14 16:08:05 -0600

    update home screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,10 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import { useLocation } from "@builder.io/qwik-city";
 import { Link } from "@builder.io/qwik-city";
 
+import { IcBaselineStart } from "~/components/icons/baseline-start";
 import { LoadingScreen } from "~/components/loading/loading-screen";
+import { Waves } from "~/components/waves/waves";
+import WallabyLogo from "~/media/Wallaby.svg?jsx";
 import { useAuthSession } from "~/routes/plugin@10-auth";
 
 export default component$(() => {
@@ -14,18 +17,26 @@ export default component$(() => {
 
   return (
     <>
-      <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content text-center">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Wallaby</h1>
-            <Link
-              prefetch={session.value !== null}
-              href="/app"
-              class="btn btn-primary"
-            >
-              Ir a Horarios
-            </Link>
+      <div class="relative h-screen w-screen">
+        <div class="relative">
+          <div class="hero min-h-screen">
+            <div class="hero-content text-center">
+              <div class="flex max-w-md flex-col gap-16">
+                <WallabyLogo />
+                <Link
+                  prefetch={session.value !== null}
+                  href="/app"
+                  class="btn btn-primary btn-lg flex items-center justify-around"
+                >
+                  <span>Ir a Horarios</span>
+                  <IcBaselineStart class="h-10 w-10" />
+                </Link>
+              </div>
+            </div>
           </div>
+        </div>
+        <div class="absolute left-0 top-0 -z-[100] h-screen overflow-hidden">
+          <Waves />
         </div>
       </div>
     </>
